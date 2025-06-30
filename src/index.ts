54eeb4c1f90e20bac5ae27b035b71c8e4eb48b33
tokkiumai <kiwidancebad@gmail.com>
2025-06-30 23:15:50 +0300

    ft: exports / 26.0.1

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,8 @@ import { Halua } from "./main"
 import { ConsoleHandler } from "./handlers/ConsoleHandler"
 
 export const halua = new Halua(ConsoleHandler())
+export type { Log, Handler } from "./handlers/types"
+export { Level } from "./handlers/types"
+export { ConsoleHandler }
+export { JSONHandler } from "./handlers/JSONHandler"
+export { TextHandler } from "./handlers/TextHandler"
