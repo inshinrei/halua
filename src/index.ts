d76d1540cd1ffa0621d95ccdc6b3253546cbc43a
tokkiumai <kiwidancebad@gmail.com>
2025-07-07 12:57:40 +0300

    rf: export HaluaLogger type / 26.0.2

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import { WebBrowserConsoleHandler } from "./handlers/WebBrowserConsoleHandler"
 
 export const halua = new Halua(WebBrowserConsoleHandler())
 export type { Log, Handler } from "./handlers/types"
+export type { HaluaLogger } from "./types"
 export { Level } from "./handlers/types"
 export { WebBrowserConsoleHandler }
 export { JSONHandler } from "./handlers/JSONHandler"
