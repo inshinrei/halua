063d576ec098eb4b5f8b576c9ed5b9b411601493
inshinrei <kiwidancebad@gmail.com>
2025-07-31 11:08:51 +0300

    WebConsoleHandler tests for message format / feat

--- src/handlers/WebConsoleHandler_test.ts
+++ src/handlers/WebConsoleHandler_test.ts
@@ -131,23 +131,88 @@ describe("WebConsoleHandler", () => {
         expect(receiver.debug).toHaveBeenCalledWith(...["%s %s %s", "abobus", "DEBUG", "log message"])
     })
 
-    test("withSeparator could be passed", () => {
-        let h = NewWebConsoleHandler(receiver, { messageFormat: "%t %l %a > %w" })
-        h.log(logWithArgs)
-        expect(receiver.debug).toHaveBeenCalledWith(
-            ...[
-                "%s %s %s %s %s %d %o %s %s %d",
-                "6/30/2025 10:54:49 PM",
-                "DEBUG",
-                "log message",
-                ">",
-                "count =",
-                2,
-                [1, 2, 3],
-                "arr",
-                "anotherCount =",
-                5,
-            ],
-        )
+    describe("messageFormat", () => {
+        test("standard", () => {
+            let format = "%t %l %a > %w"
+            NewWebConsoleHandler(receiver, { messageFormat: format }).log(logWithArgs)
+            expect(receiver.debug).toHaveBeenCalledWith(
+                ...[
+                    "%s %s %s %s %s %d %o %s %s %d",
+                    "6/30/2025 10:54:49 PM",
+                    "DEBUG",
+                    "log message",
+                    ">",
+                    "count =",
+                    2,
+                    [1, 2, 3],
+                    "arr",
+                    "anotherCount =",
+                    5,
+                ],
+            )
+        })
+
+        test("remove time and level", () => {
+            let format = "%a > %w"
+            NewWebConsoleHandler(receiver, { messageFormat: format }).log(logWithArgs)
+            expect(receiver.debug).toHaveBeenCalledWith(
+                ...["%s %s %s %d %o %s %s %d", "log message", ">", "count", 2, [1, 2, 3], "arr", "anotherCount =", 5],
+            )
+        })
+
+        test("absent withArgs does remove separator", () => {
+            let format = "%t %l %a >> %w"
+            NewWebConsoleHandler(receiver, { messageFormat: format }).log(log)
+            expect(receiver.debug).toHaveBeenCalledWith(
+                ...["%s %s %s", "6/30/2025 10:54:49 PM", "DEBUG", "log message"],
+            )
+        })
+
+        test("prefixed", () => {
+            let format = "[prefixed] %t %l %a > %w"
+            NewWebConsoleHandler(receiver, { messageFormat: format }).log(logWithArgs)
+            expect(receiver.debug).toHaveBeenCalledWith(
+                ...[
+                    "%s %s %s %s %s %s %d %o %s %s %d",
+                    "[prefixed]",
+                    "6/30/2025 10:54:49 PM",
+                    "DEBUG",
+                    "log message",
+                    ">",
+                    "count =",
+                    2,
+                    [1, 2, 3],
+                    "arr",
+                    "anotherCount =",
+                    5,
+                ],
+            )
+        })
+
+        test("no spaces", () => {
+            let format = "%t%l%a%w"
+            NewWebConsoleHandler(receiver, { messageFormat: format }).log(log)
+            expect(receiver.debug).toHaveBeenCalledWith()
+        })
+
+        test("args first", () => {
+            let format = "%a %w > %t %l"
+            NewWebConsoleHandler(receiver, { messageFormat: format }).log(log)
+            expect(receiver.debug).toHaveBeenCalledWith()
+        })
+
+        test("separator change", () => {
+            let format = "%t %l %a ::: %w"
+            NewWebConsoleHandler(receiver, { messageFormat: format }).log(log)
+            expect(receiver.debug).toHaveBeenCalledWith()
+        })
+
+        test("adding staff", () => {
+            let format = "[time] %t on level %l with args %a and with %w"
+            NewWebConsoleHandler(receiver, { messageFormat: format }).log(log)
+            expect(receiver.debug).toHaveBeenCalledWith()
+        })
     })
+
+    describe("messageFormatPretty", () => {})
 })
