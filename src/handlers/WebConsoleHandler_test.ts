4802b1083b4a09e278019db96e7936be982734d3
inshinrei <kiwidancebad@gmail.com>
2025-08-07 23:44:23 +0300

    message format for WebConsoleHandler pretty option / feat

--- src/handlers/WebConsoleHandler_test.ts
+++ src/handlers/WebConsoleHandler_test.ts
@@ -275,5 +275,217 @@ describe("WebConsoleHandler", () => {
         })
     })
 
-    describe("messageFormatPretty", () => {})
+    describe("messageFormatPretty", () => {
+        test("standard", () => {
+            let format = "%t %l %a > %w"
+            NewWebConsoleHandler(receiver, {
+                messageFormat: format,
+                pretty: true,
+                fetchBrowserThemeOnInstanceCreation: false,
+            }).log(logWithArgs)
+            expect(receiver.debug).toHaveBeenCalledWith(
+                ...[
+                    "%s %s %s %s %d %o %s %s %d",
+                    "%c6/30/2025 10:54:49 PM %cDEBUG%c",
+                    "color:#565656",
+                    "color:#8A228A",
+                    "color:#224912",
+                    "log message",
+                    ">",
+                    "count =",
+                    2,
+                    [1, 2, 3],
+                    "arr",
+                    "anotherCount =",
+                    5,
+                ],
+            )
+        })
+
+        test("remove time and level", () => {
+            let format = "%a > %w"
+            NewWebConsoleHandler(receiver, {
+                messageFormat: format,
+                pretty: true,
+                fetchBrowserThemeOnInstanceCreation: false,
+            }).log(logWithArgs)
+            expect(receiver.debug).toHaveBeenCalledWith(
+                ...[
+                    "%s %s %d %o %s %s %d",
+                    "%c",
+                    "color:#224912",
+                    "log message",
+                    ">",
+                    "count =",
+                    2,
+                    [1, 2, 3],
+                    "arr",
+                    "anotherCount =",
+                    5,
+                ],
+            )
+        })
+
+        test("absent withArgs does remove separator", () => {
+            let format = "%t %l %a >> %w"
+            NewWebConsoleHandler(receiver, {
+                messageFormat: format,
+                pretty: true,
+                fetchBrowserThemeOnInstanceCreation: false,
+            }).log(log)
+            expect(receiver.debug).toHaveBeenCalledWith(
+                ...[
+                    "%s %s",
+                    "%c6/30/2025 10:54:49 PM %cDEBUG%c",
+                    "color:#565656",
+                    "color:#8A228A",
+                    "color:#224912",
+                    "log message",
+                ],
+            )
+        })
+
+        test.todo("prefixed", () => {
+            let format = "[prefixed] %t %l %a > %w"
+            NewWebConsoleHandler(receiver, {
+                messageFormat: format,
+                pretty: true,
+                fetchBrowserThemeOnInstanceCreation: false,
+            }).log(logWithArgs)
+            expect(receiver.debug).toHaveBeenCalledWith(
+                ...[
+                    "%s %s %s %s %s %d %o %s %s %d",
+                    "%c6/30/2025 10:54:49 PM %cDEBUG%c",
+                    "color:#565656",
+                    "color:#8A228A",
+                    "color:#224912",
+                    "[prefixed]",
+                    "6/30/2025 10:54:49 PM",
+                    "DEBUG",
+                    "log message",
+                    ">",
+                    "count =",
+                    2,
+                    [1, 2, 3],
+                    "arr",
+                    "anotherCount =",
+                    5,
+                ],
+            )
+        })
+
+        test("no spaces", () => {
+            let format = "%t%l%a%w"
+            NewWebConsoleHandler(receiver, {
+                messageFormat: format,
+                pretty: true,
+                fetchBrowserThemeOnInstanceCreation: false,
+            }).log(logWithArgs)
+            expect(receiver.debug).toHaveBeenCalledWith(
+                ...[
+                    "%s %s %s %d %o %s %s %d",
+                    "%c6/30/2025 10:54:49 PM %cDEBUG%c",
+                    "color:#565656",
+                    "color:#8A228A",
+                    "color:#224912",
+                    "log message",
+                    "count =",
+                    2,
+                    [1, 2, 3],
+                    "arr",
+                    "anotherCount =",
+                    5,
+                ],
+            )
+        })
+
+        test.todo("args first", () => {
+            let format = "%a %w > %t %l"
+            NewWebConsoleHandler(receiver, {
+                messageFormat: format,
+                pretty: true,
+                fetchBrowserThemeOnInstanceCreation: false,
+            }).log(logWithArgs)
+            expect(receiver.debug).toHaveBeenCalledWith(
+                ...[
+                    "%s %s %s %d %o %s %s %d %s",
+                    "%c%c6/30/2025 10:54:49 PM %cDEBUG",
+                    "color:#224912",
+                    "color:#565656",
+                    "color:#8A228A",
+
+                    "log message",
+                    "count",
+                    2,
+                    [1, 2, 3],
+                    "arr",
+                    "anotherCount =",
+                    5,
+                    ">",
+                    "6/30/2025 10:54:49 PM",
+                    "DEBUG",
+                ],
+            )
+        })
+
+        test("separator change", () => {
+            let format = "%t %l %a ::: %w"
+            NewWebConsoleHandler(receiver, {
+                messageFormat: format,
+                pretty: true,
+                fetchBrowserThemeOnInstanceCreation: false,
+            }).log(logWithArgs)
+            expect(receiver.debug).toHaveBeenCalledWith(
+                ...[
+                    "%s %s %s %s %d %o %s %s %d",
+                    "%c6/30/2025 10:54:49 PM %cDEBUG%c",
+                    "color:#565656",
+                    "color:#8A228A",
+                    "color:#224912",
+                    "log message",
+                    ":::",
+                    "count =",
+                    2,
+                    [1, 2, 3],
+                    "arr",
+                    "anotherCount =",
+                    5,
+                ],
+            )
+        })
+
+        test.todo("adding staff", () => {
+            let format = "[time] %t on level %l with args %a and with %w"
+            NewWebConsoleHandler(receiver, {
+                messageFormat: format,
+                pretty: true,
+                fetchBrowserThemeOnInstanceCreation: false,
+            }).log(logWithArgs)
+            expect(receiver.debug).toHaveBeenCalledWith(
+                ...[
+                    "%s %s %s %s %s %s %s %s %s %s %d %o %s %s %d",
+                    "%c6/30/2025 10:54:49 PM %cDEBUG%c",
+                    "color:#565656",
+                    "color:#8A228A",
+                    "color:#224912",
+                    "[time]",
+                    "6/30/2025 10:54:49 PM",
+                    "on",
+                    "level",
+                    "DEBUG",
+                    "with",
+                    "args",
+                    "log message",
+                    "and",
+                    "with",
+                    "count =",
+                    2,
+                    [1, 2, 3],
+                    "arr",
+                    "anotherCount =",
+                    5,
+                ],
+            )
+        })
+    })
 })
