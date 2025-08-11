1c708f6897f5f32a709c4c05856317c8d4b1680f
inshinrei <kiwidancebad@gmail.com>
2025-08-11 20:41:33 +0300

    main tests for instance creation / feat

--- src/main_test.ts
+++ src/main_test.ts
@@ -6,7 +6,7 @@ import type { HaluaLogger } from "./types"
 describe("Halua Logger", () => {
     let halua = new Halua([])
 
-    function setupHandler(receiver: any) {
+    function makeHandler(receiver: any) {
         return () => receiver
     }
 
@@ -19,28 +19,21 @@ describe("Halua Logger", () => {
 
     afterEach(vi.clearAllMocks)
 
-    test("creates new instance", () => {
-        let logger = halua.New()
-        let loggerWith = halua.With()
-        expect(Object.is(logger, halua)).toBeFalsy()
-        expect(Object.is(loggerWith, halua)).toBeFalsy()
-    })
-
     test("invokes handler", () => {
-        let logger = halua.New(setupHandler(r1))
+        let logger = halua.New(makeHandler(r1))
         logger.info("logs info")
         expect(r1.log).toHaveBeenCalledTimes(1)
     })
 
     test("creates new instance with multiple handlers", () => {
-        let logger = halua.New([setupHandler(r1), setupHandler(r2)])
+        let logger = halua.New([makeHandler(r1), makeHandler(r2)])
         logger.info("logs")
         expect(r1.log).toHaveBeenCalledTimes(1)
         expect(r2.log).toHaveBeenCalledTimes(1)
     })
 
     test("creates new instance with options as first argument, inherits the handler", () => {
-        let logger = halua.New(setupHandler(r1))
+        let logger = halua.New(makeHandler(r1))
         let logger2 = logger.New({ minLevel: Level.Debug })
         logger2.info("logs")
         expect(r1.log).toHaveBeenCalledTimes(1)
@@ -54,10 +47,10 @@ describe("Halua Logger", () => {
             l.error("logs error")
         }
 
-        log(halua.New(setupHandler(r1), { minLevel: Level.Debug }))
-        log(halua.New(setupHandler(r1), { minLevel: Level.Info }))
-        log(halua.New(setupHandler(r1), { minLevel: Level.Warn }))
-        log(halua.New(setupHandler(r1), { minLevel: Level.Error }))
+        log(halua.New(makeHandler(r1), { minLevel: Level.Debug }))
+        log(halua.New(makeHandler(r1), { minLevel: Level.Info }))
+        log(halua.New(makeHandler(r1), { minLevel: Level.Warn }))
+        log(halua.New(makeHandler(r1), { minLevel: Level.Error }))
         expect(r1.log).toHaveBeenNthCalledWith(1, expect.objectContaining({ level: Level.Debug }))
         expect(r1.log).toHaveBeenNthCalledWith(2, expect.objectContaining({ level: Level.Info }))
         expect(r1.log).toHaveBeenNthCalledWith(3, expect.objectContaining({ level: Level.Warn }))
@@ -66,13 +59,13 @@ describe("Halua Logger", () => {
 
     test("appends withArgs by With method", () => {
         let withArgs = ["string", [1, 2, 3], 1]
-        let logger = halua.New(setupHandler(r1)).With(...withArgs)
+        let logger = halua.New(makeHandler(r1)).With(...withArgs)
         logger.info("logs")
         expect(r1.log).toHaveBeenCalledWith(expect.objectContaining({ withArgs }))
     })
 
     test("do not false assert", () => {
-        let logger = halua.New(setupHandler(r1))
+        let logger = halua.New(makeHandler(r1))
         logger.assert(true, "assertion")
         expect(r1.log).not.toHaveBeenCalled()
         logger.assert(false, "assertion")
@@ -80,8 +73,46 @@ describe("Halua Logger", () => {
     })
 
     test("message format change", () => {
-        let logger = halua.New(setupHandler(r1)).withMessageFormat("%l %a")
+        let logger = halua.New(makeHandler(r1)).withMessageFormat("%l %a")
         logger.info("logs info")
         expect(r1.log).toHaveBeenCalledWith(expect.objectContaining({ messageFormat: "%l %a" }))
+    })
+
+    describe("instance creation", () => {
+        test("creates instance by calling New", () => {
+            let logger = halua.New(makeHandler(r1))
+            let logger2 = halua.New([makeHandler(r1), makeHandler(r2)])
+            let logger3 = halua.With("string")
+            expect(Object.is(logger, halua)).toBeFalsy()
+            expect(Object.is(logger2, halua)).toBeFalsy()
+            expect(Object.is(logger3, halua)).toBeFalsy()
+        })
+
+        test("implies handlers of previous instance", () => {
+            let logger = halua.New(makeHandler(r1))
+            let nextLogger = logger.New()
+            nextLogger.info("logs")
+            expect(r1.log).toHaveBeenCalledTimes(1)
+        })
+
+        test("new instance does not point on prev handlers", () => {
+            let logger = halua.New(makeHandler(r1))
+            let nextLogger = logger.New()
+            logger.setHandler(makeHandler(r2))
+            logger.info("logger log")
+            nextLogger.info("nextLogger log")
+            expect(r1.log).toHaveBeenCalledTimes(1)
+            expect(r2.log).toHaveBeenCalledTimes(1)
+        })
+
+        test("new instance does not point on prev options", () => {
+            let logger = halua.New(makeHandler(r1), { messageFormat: "%t %l" })
+            let nextLogger = logger.New()
+            nextLogger.withMessageFormat("%l %t")
+            nextLogger.info("nextLogger log")
+            expect(r1.log).toHaveBeenCalledWith(expect.objectContaining({ messageFormat: "%l %t" }))
+            logger.info("logger log")
+            expect(r1.log).toHaveBeenCalledWith(expect.objectContaining({ messageFormat: "%t %l" }))
+        })
     })
 })
