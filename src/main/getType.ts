a6ddb564e3c7e8d03356cca9d71d370c771a72b5
inshinrei <kiwidancebad@gmail.com>
2025-10-22 20:17:37 +0300

    getType func / feat

---
+++ src/main/getType.ts
@@ -0,0 +1,63 @@
+export type ArgumentType =
+    | "undefined"
+    | "null"
+    | "string"
+    | "number"
+    | "array"
+    | "object"
+    | "symbol"
+    | "typedarray"
+    | "arraybuffer"
+    | "bigint"
+    | "map"
+    | "set"
+    | "weakmap"
+    | "weakset"
+    | "date"
+    | "nan"
+    | "infinity"
+    | "function"
+    | "error"
+
+export function getType(arg: any): ArgumentType {
+    if (typeof arg === "undefined") {
+        return "undefined"
+    }
+    if (typeof arg === "object" && String(arg) === "null") {
+        return "null"
+    }
+    if (Array.isArray(arg)) {
+        return "array"
+    }
+    if (ArrayBuffer.isView(arg)) {
+        return "typedarray"
+    }
+    if (arg instanceof ArrayBuffer) {
+        return "arraybuffer"
+    }
+    if (arg instanceof Map) {
+        return "map"
+    }
+    if (arg instanceof Set) {
+        return "set"
+    }
+    if (arg instanceof WeakMap) {
+        return "weakmap"
+    }
+    if (arg instanceof WeakSet) {
+        return "weakset"
+    }
+    if (arg instanceof Date) {
+        return "date"
+    }
+    if (arg instanceof Error) {
+        return "error"
+    }
+    if (Number.isNaN(arg)) {
+        return "nan"
+    }
+    if (typeof arg === "number" && !Number.isFinite(arg)) {
+        return "infinity"
+    }
+    return typeof arg as ArgumentType
+}
