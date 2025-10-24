a731c0c2c10dac5b3dea8469f8e98105b984e487
inshinrei <kiwidancebad@gmail.com>
2025-10-24 13:09:37 +0300

    handler types / feat

--- src/main/handlers/HandlerBase.ts
+++ src/main/handlers/HandlerBase.ts
@@ -14,9 +14,11 @@ export class HandlerBase implements Handler {
             if (current.type === "init") {
                 yield { type: "init" }
             }
+
             if (current.prev) {
                 arg += current.prev
             }
+            
             if (current.type === "arg") {
                 if (typeof this.formatArg === "function") {
                     arg += (this.formatArg as Function)(current.value)
