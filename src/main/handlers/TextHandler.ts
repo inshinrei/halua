e6c1ed540753a3a00b1a9c65862493542a98a0be
inshinrei <kiwidancebad@gmail.com>
2025-12-23 20:16:12 +0300

    reworked TextHandler / feat

--- src/main/handlers/TextHandler.ts
+++ src/main/handlers/TextHandler.ts
@@ -2,12 +2,17 @@ import { HandlerBase, SendMethod } from "./HandlerBase"
 import { LogLevel } from "../../types/log"
 import { format } from "../format"
 import { getType } from "../getType"
+import { toarray } from "../util/cast"
 
 interface TextLogHandlerOptions {
-    level: LogLevel
+    level?: LogLevel
+    exact?: LogLevel | Array<LogLevel>
+    spacing?: boolean
+    printTimestamp?: boolean
+    printLevel?: boolean
 }
 
-export function NewTextHandler(send: (data: string) => void, options: TextLogHandlerOptions) {
+export function NewTextHandler(send: (data: string) => void, options?: TextLogHandlerOptions) {
     return () =>
         new (class TextHandler extends HandlerBase {
             public level?: LogLevel
@@ -15,12 +20,18 @@ export function NewTextHandler(send: (data: string) => void, options: TextLogHan
 
             readonly formatArg
 
-            constructor(send: SendMethod, options: TextLogHandlerOptions) {
+            constructor(send: SendMethod, options: TextLogHandlerOptions = {}) {
                 super(send)
-                this.level = options.level
-                // this.exact = arrayed(exact)
+                this.applyOptions(options)
 
                 this.formatArg = (value: any) => format({ type: getType(value), value })
+            }
+
+            applyOptions(options: TextLogHandlerOptions) {
+                this.level = options.level ?? "TRACE"
+                this.exact = toarray(options?.exact ?? []) as Array<LogLevel>
+                this.printTimestamp = options.printTimestamp ?? true
+                this.printLevel = options.printLevel ?? true
             }
         })(send, options)
 }
