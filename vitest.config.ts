2a7e34fbf9bfdb9df071020331f1c13e2336ce45
inshinrei <kiwidancebad@gmail.com>
2026-05-22 21:53:25 +0300

    refactor: rename test files _test -> _unit

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,6 +3,6 @@ import { defineConfig } from "vitest/config"
 export default defineConfig({
     test: {
         globalSetup: "./src/vitest.global-setup.ts",
-        include: ["src/**/*_test.?(c|m)[jt]s?(x)"],
+        include: ["src/**/*_unit.?(c|m)[jt]s?(x)"],
     },
 })
